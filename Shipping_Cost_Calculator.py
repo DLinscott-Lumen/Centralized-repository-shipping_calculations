209dd3a4ec81401c8214dc8366daeb301d79dfed
David Linscott <linscott.david@gmail.com>
2025-09-30 23:36:20 -0400

    added files and adjusted calculator

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,3 +10,6 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
+## Provide Weight in Pounds
+weight_lbs = weight * 2.20462
+print(f"Weight in pounds: {weight_lbs}")
